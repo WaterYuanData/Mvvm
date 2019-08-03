552e8446cc66ca15dbc3c2eb136c6d8857581b07
WaterYuan <water.yuan.data@gmail.com>
2019-08-03 22:56:51 +0800

    ViemModel对延迟加载有效，

--- app/src/main/java/com/yuan/mvvm/viewmodel/Main2Activity.java
+++ app/src/main/java/com/yuan/mvvm/viewmodel/Main2Activity.java
@@ -2,6 +2,8 @@ package com.yuan.mvvm.viewmodel;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ObservableArrayList;
+import androidx.databinding.ObservableList;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -9,6 +11,7 @@ import androidx.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
 
 import com.yuan.mvvm.R;
@@ -20,16 +23,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main2Activity extends AppCompatActivity {
-
+    private static final String TAG = "Main2Activity";
     private StudentViewModel mStudentViewModel;
     private ActivityMain2Binding mBinding;
+    private ObservableList<Student> mStudent_binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 绑定xml
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main2);
+
+        // 将点击事件绑定到xml中
         // todo 遗漏导致点击无效
         mBinding.setMyClick(new MyClick());
+
+        // 将数据绑定到xml中
+        mStudent_binding = new ObservableArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            Student student = new Student();
+            student.setName("y" + SystemClock.currentThreadTimeMillis() % 7777);
+            mStudent_binding.add(student);
+        }
+        mBinding.setStudent1(mStudent_binding);
+        mBinding.setStudent2(mStudent_binding);
 
         mStudentViewModel = ViewModelProviders.of(this).get(StudentViewModel.class);
         mStudentViewModel.getStudents().observe(this, new Observer<List<Student>>() {
@@ -40,11 +57,16 @@ public class Main2Activity extends AppCompatActivity {
                     string = string + "\n" + students.get(i).toString();
                 }
                 mBinding.tvStudentInfo.setText(string);
+
+                mBinding.setStudent1(mStudent_binding);
+                mBinding.setStudent2(mStudent_binding);
             }
         });
     }
 
     public void updateStudentInfo(View view) {
+        // 点击更新数据属于业务逻辑应放在ViewModel中处理
+
         final MutableLiveData<List<Student>> students = mStudentViewModel.getStudents();
         // List<Student> studentList = students.getValue(); // 得到null
         final List<Student> studentList = new ArrayList<>();
@@ -64,5 +86,11 @@ public class Main2Activity extends AppCompatActivity {
                 students.postValue(studentList);
             }
         }, 1500);
+
+        mStudent_binding.clear();
+        mStudent_binding.addAll(studentList);
+        for (int i = 0; i < mStudent_binding.size(); i++) { // 因为延迟，此时mStudent_binding.size()永远是1
+            Log.d(TAG, "updateStudentInfo: " + mStudent_binding.get(i));
+        }
     }
 }
